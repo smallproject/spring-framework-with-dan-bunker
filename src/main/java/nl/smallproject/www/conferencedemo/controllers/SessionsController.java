56cf8525a968d96884d6473eaaf8a5f43a3c637d
smallproject <j.a.villas@live.com.ph>
2024-08-05 20:09:00 +0200

    created spring API REST controllers

---
+++ src/main/java/nl/smallproject/www/conferencedemo/controllers/SessionsController.java
@@ -0,0 +1,54 @@
+package nl.smallproject.www.conferencedemo.controllers;
+
+import nl.smallproject.www.conferencedemo.models.Session;
+import nl.smallproject.www.conferencedemo.repositories.SessionRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+//proper http return status
+//import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/sessions")
+public class SessionsController {
+    @Autowired
+    private SessionRepository sessionsRepository;
+    @Autowired
+    private SessionRepository sessionRepository;
+
+    @GetMapping
+    public List<Session> list() {
+        return sessionsRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Session get(@PathVariable Long id) {
+        return sessionsRepository.getOne(id);
+    }
+
+//    returns standard spring status mapping 200
+    @PostMapping
+//    proper http return status
+//    @ResponseStatus(HttpStatus.CREATED)
+    public Session create(@RequestBody final Session session) {
+        return sessionsRepository.saveAndFlush(session);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        //Also need to check for the children records before deleting.
+        sessionsRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Session update(@PathVariable Long id, @RequestBody Session session) {
+//        because this is a PUT, we expect all attributes to be passed in. A PATCH would only need what
+        //TODO: Add validation that all attributes are passed in, otherwise return a 400 bad payload
+        Session existingSession = sessionsRepository.getOne(id);
+        BeanUtils.copyProperties(session, existingSession, "session_id");
+        return sessionRepository.saveAndFlush(existingSession);
+    }
+}
---
+++ src/main/java/nl/smallproject/www/conferencedemo/controllers/SpeakerController.java
@@ -0,0 +1,54 @@
+package nl.smallproject.www.conferencedemo.controllers;
+
+import nl.smallproject.www.conferencedemo.models.Speaker;
+import nl.smallproject.www.conferencedemo.repositories.SessionRepository;
+import nl.smallproject.www.conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+//proper http return status
+//import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/speakers")
+public class SpeakerController {
+    @Autowired
+    private SpeakerRepository speakersRepository;
+    private SessionRepository sessionRepository;
+
+    @GetMapping
+    public List<Speaker> list() {
+        return speakersRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Speaker get(@PathVariable Long id) {
+        return speakersRepository.getOne(id);
+    }
+
+//    returns standard spring status mapping 200
+    @PostMapping
+//    proper http return status
+//    @ResponseStatus(HttpStatus.CREATED)
+    public Speaker create(@RequestBody final Speaker speaker) {
+        return speakersRepository.saveAndFlush(speaker);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        //Also need to check for the children records before deleting.
+        speakersRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker) {
+//        because this is a PUT, we expect all attributes to be passed in. A PATCH would only need what
+        //TODO: Add validation that all attributes are passed in, otherwise return a 400 bad payload
+        Speaker existingSpeaker = speakersRepository.getOne(id);
+        BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
+        return speakersRepository.saveAndFlush(existingSpeaker);
+    }
+}
