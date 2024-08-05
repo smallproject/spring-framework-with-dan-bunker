5e25b75d595861b323c4bc77038cc9b553bd6a01
smallproject <j.a.villas@live.com.ph>
2024-08-05 19:09:31 +0200

    working with jpa repositories

--- src/main/java/nl/smallproject/www/conferencedemo/repositories/SessionRepository.java
+++ src/main/java/nl/smallproject/www/conferencedemo/repositories/SessionRepository.java
@@ -1,2 +1,7 @@
-package nl.smallproject.www.conferencedemo.repositories;public class SessionRepository {
+package nl.smallproject.www.conferencedemo.repositories;
+
+import nl.smallproject.www.conferencedemo.models.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SessionRepository extends JpaRepository<Session, Long> {
 }
--- src/main/java/nl/smallproject/www/conferencedemo/repositories/SpeakerRepository.java
+++ src/main/java/nl/smallproject/www/conferencedemo/repositories/SpeakerRepository.java
@@ -1,2 +1,7 @@
-package nl.smallproject.www.conferencedemo.repositories;public interface SpeakerRepository {
+package nl.smallproject.www.conferencedemo.repositories;
+
+import nl.smallproject.www.conferencedemo.models.Speaker;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpeakerRepository extends JpaRepository<Speaker, Long> {
 }
